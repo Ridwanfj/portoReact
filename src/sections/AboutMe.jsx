80250038ca81a9b1534a604121f40aaf5ced3bda
Ridwan Fajariansyah <147936429+Ridwanfj@users.noreply.github.com>
2025-11-23 06:33:50 +0700

    cv

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -145,7 +145,8 @@ export default function AboutMe() {
           </p>
 
           <motion.a
-            href="/cv.pdf"
+            href="https://drive.google.com/file/d/1ts0KXzitIOZPGsFu_4VT9_MI6i5hJDMJ/view?usp=sharing"
+            target="blank"
             download
             className="
               inline-block mt-2 md:mt-4
